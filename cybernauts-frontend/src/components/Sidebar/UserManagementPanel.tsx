a37f10256d6e9e8a1c8f7000165bb35eb4e9044e
ramkrishna-behera <ramkrishnabehera1999@gmail.com>
2025-10-29 01:50:58 +0530

    Build Fix

--- cybernauts-frontend/src/components/Sidebar/UserManagementPanel.tsx
+++ cybernauts-frontend/src/components/Sidebar/UserManagementPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { useUsers } from "../../context/useUsers";
 import toast from "react-hot-toast";
 
